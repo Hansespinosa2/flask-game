381ee0ff15df80557712d3502583ca56b8c65251
Hansespinosa2 <andresespinosa@ufl.edu>
2024-03-10 13:37:00 -0500

    Add new game creator

--- game.rb
+++ game.rb
@@ -1,8 +1,9 @@
-require_relative 'colors.rb'
+
 
 class Game
   def initialize
     @flasks = []
+    @possible_colors = ['red', 'blue','green', 'yellow', 'purple', 'orange']
   end
 
   def add_flask(flask)
@@ -11,12 +12,18 @@ class Game
 
   def create_game(n_colors=nil)
     if n_colors.nil?
-      n_colors = rand(4..colors.length)
+      n_colors = rand(4..@possible_colors.length)
+      @possible_colors = @possible_colors.sample(n_colors)
     end
 
-    n_colors + 1.times do
-      colors = (possible_colors *4).shuffle
+    colors = (@possible_colors *4).shuffle
+
+    (n_colors + 1).times do
       flask = Flask.new
+      flask.add_liquid(colors.pop(4))
+      print(colors)
+      @flasks << flask
+    end
 
 
 
